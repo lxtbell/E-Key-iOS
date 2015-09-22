c9ca6c2862806286da4415a926e371e8c16bc0f6
SamYang95 <yangwx01@yeah.net>
2015-09-22 09:19:00 -0500

    space-to-work-in
    This is an empty file to provide members with space to work in

---
+++ EKey/EKey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EKey
+//
+//  Created by SamYang95 on 9/22/15.
+//  Copyright © 2015 SamYang95. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
